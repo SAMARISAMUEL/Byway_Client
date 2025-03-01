2a9ddcdcadfb2fdac3acd34529ef7809e910cd1e
samari samuel <liveogd@yahoo.com>
2025-03-01 16:08:25 +0100

    feat : Setup for LMS Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-700">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
